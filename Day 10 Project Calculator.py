0af8f6ca42ae5672f48165b96f271cf006e29baa
Anietie Akpanusoh <105338623+aniusoro@users.noreply.github.com>
2022-06-04 14:29:39 -0600

    Update Day 10 Project Calculator.py

--- Day 10 Project Calculator.py
+++ Day 10 Project Calculator.py
@@ -1,3 +1,9 @@
+"""This is a Python program of  avery basic calculator that is able to continue manipulating the current answer
+   until the user is done with it and wants to begin a new calculation
+"""
+
+
+#The four functions below are all functions for the four basic arithmetic methods
 def add(n1, n2):
     return n1 + n2
 
@@ -10,23 +16,32 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
+"""This is a dictionary where the key is the arithmetic operator 
+and its value is its coresponding function from the four stated above """
 operations = {
     "+": add,
     "-": subtract,
     "*": multiply,
     "/": divide
 }
+
+#this is the recursive calculator function
 def calculator():
 
+#here the user is asked for the initial number to begin his calculation
     num1 = float(input("What is the first number? "))
     for symbol in operations:
         print(symbol)
-
+#"should_continue" will mark whether the user wants to continue solving with the current answer thus keeping the while loop running   
     should_continue = True
     while should_continue:
+# user picks the operator they would like to use
         operation_symbol = input("Pick an operation: ")
+# user picks the second number 
         num2 = float(input("What is the next number?: "))
+
         answer = 0
+# for each operator chosen, the appropriate function is called
         if operation_symbol == "+":
             answer = add(num1, num2)
         elif operation_symbol == "-":
@@ -37,12 +52,17 @@ def calculator():
             answer = divide(num1, num2)
         
         print(f"{num1} {operation_symbol} {num2} = {answer}")
-        
+ 
+# this asks if the user would like to continue solving with the current answer
+
         decision = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation.: ")
         if decision == 'y':
             num1 = answer
         else:
             should_continue = False
+ #if the user is done, then the function calls itself and starts again 
             calculator()
 
+    
+# function call 
 calculator()
