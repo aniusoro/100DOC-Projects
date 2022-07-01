d5b930a54d2eed0f328ee5725783b5a7dbc5c0e7
Anietie Akpanusoh <105338623+aniusoro@users.noreply.github.com>
2022-07-01 14:24:12 -0600

    Update day-18-project-polygons.py

--- day-18-project-polygons.py
+++ day-18-project-polygons.py
@@ -5,9 +5,13 @@ import random
 timmy = Turtle()
 timmy.shape("arrow")
 
+"""the list of colors that the shapes will have their color randomly selected from"""
 colors = ["green", "red", "blue", "pink", "DarkGreen"]
+
+#starting with a triangle, the starting number of sides is 3
 sides = 3
 
+#this will mark the start and end of the drawing process
 draw = True
 while draw:
     the_color = random.choice(colors)
@@ -15,36 +19,15 @@ while draw:
         angle = 360 / sides
         timmy.forward(100)
         timmy.right(angle)
+        
+    #increases the number of sides by 1 after a shape has been drawn
     sides += 1
+    #the color is then changed
     timmy.color(the_color)
+    #once the number of sides reaches 12, the while loop will end
     if sides == 12:
+    #the loop will end when this is true
         draw = False
-
-
-# for i in range(sides):
-#     timmy.forward(100)
-#     timmy.right(90)
-# sides += 1
-#
-# for i in range(sides):
-#     timmy.forward(100)
-#     timmy.right(72)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 screen = Screen()
