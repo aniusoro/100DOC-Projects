063a068545bc0375510c255d26f62b30d41f49cc
Anietie Akpanusoh <105338623+aniusoro@users.noreply.github.com>
2022-07-11 22:37:55 +0100

    Add files via upload

---
+++ Day 20/snake.py
@@ -0,0 +1,54 @@
+from turtle import Turtle
+"""using tuples I names the coordinates for the three segments of the snake"""
+COORDINATES = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in COORDINATES:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        snake = Turtle(shape="square")
+        snake.color("white")
+        snake.penup()  # this eliminates the path of the snakes being drawn
+        snake.goto(position)
+        self.segments.append(snake)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        # this is to move segments to the position of the one before it starting from the tail to the head
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            # setting the new coordinates for where the segments are to move to
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+    # after all the code above has been implemented, then we can now move the first segment forward
+    # the other segments take the position of the one that preceeded it.
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right (self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
