69f82effdb6ca9949817ca1a8fc0c661521719d7
Anietie Akpanusoh <105338623+aniusoro@users.noreply.github.com>
2022-08-02 12:59:46 -0400

    Update Paddle.py

--- Day 22/Paddle.py
+++ Day 22/Paddle.py
@@ -1,46 +1,21 @@
 from turtle import Turtle
-COORDINATES1 = [(350, 0), (350, 20), (350, 40)]
-COORDINATES2 = [(-350, 0), (-350, 20), (-350, 40)]
-MOVE_DISTANCE = 20
-UP = 90
-DOWN = 270
 
-
-class Paddle1():
-
-    def __init__(self):
-        self.segment1 = []
-        self.create_paddle1()
-        self.head = self.segment1[0]
-
-    def create_paddle1(self):
-        for position in COORDINATES1:
-            self.add_segment(position)
-
-    def add_segment(self, position):
-        paddle = Turtle(shape="square")
-        paddle.color("white")
-        paddle.penup()
-        paddle.goto(position)
-        paddle.shapesize(stretch_wid=5, stretch_len=1)
-        self.segment1.append(paddle)
-
-
-class Paddle2():
-    def __init__(self):
-        self.segment2 = []
-        self.create_paddle2()
-        self.head = self.segment2[0]
-
-    def create_paddle2(self):
-        for position in COORDINATES2:
-            self.add_segment(position)
-
-    def add_segment(self, position):
-        paddle = Turtle(shape="square")
-        paddle.color("white")
-        paddle.penup()
-        paddle.goto(position)
-        paddle.shapesize(stretch_wid=5, stretch_len=1)
-        self.segment2.append(paddle)
-
+class Paddle(Turtle):
+    
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
+    
+    def go_up():
+        new_y = self.ycor + 20
+        self.goto(self.xcor, new_y)
+    
+    def go_down():
+        new_y = self.ycor - 20
+        self.goto(self.xcor, new_y)
+    
+    
