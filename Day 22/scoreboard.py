8a6dcc0ade7d5d217d1ea6cbec3b49a25069ccbd
Anietie Akpanusoh <105338623+aniusoro@users.noreply.github.com>
2022-08-02 13:06:08 -0400

    Update scoreboard.py

--- Day 22/scoreboard.py
+++ Day 22/scoreboard.py
@@ -1,28 +1,26 @@
 from turtle import Turtle
-ALIGNMENT = "center"
-FONT = ("Courier", 24, "normal")
-COORDINATES = [(250, 250), (-250, 250)]
-
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score1 = 0
-        self.score2 = 0
         self.color("white")
-        self.penup()
-        self.goto(0, 250)
+        self.penup
         self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"{self.score1} : {self.score2}", move=False, align=ALIGNMENT, font=FONT)
-
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
-
-    def increase_score(self):
-        self.score += 1
         self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, align = "center", font = ("Courier", 80, "normal")
+        self.goto(100, 200)
+        self.write(self.r_score, align = "center", font = ("Courier", 80, "normal")
+
+    def l_point(self):
+        self.l_score += 1
+        self.update_scoreboard()
+
+    def r_point(self):
+        self.l_score += 1
         self.update_scoreboard()
