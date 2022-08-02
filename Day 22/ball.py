951d5a1f6dfd707c8a3b63b50ddd8c338883b356
Anietie Akpanusoh <105338623+aniusoro@users.noreply.github.com>
2022-08-02 13:32:13 -0400

    Update ball.py

--- Day 22/ball.py
+++ Day 22/ball.py
@@ -21,9 +21,11 @@ class Ball(Turtle):
   
   def bounce_x(self):
     self.x_move *= -1
+    self.move_speed *= 0.7
   
   def reset_position(self):
     self.goto(0,0)
+    self.move_speed = 0.1
     self.bounce_x()
  
   
