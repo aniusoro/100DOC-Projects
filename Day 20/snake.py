8d734a67ce63cf829d57db1aa971b42f455c8061
Anietie Akpanusoh <105338623+aniusoro@users.noreply.github.com>
2022-08-11 14:13:17 -0400

    Update snake.py

--- Day 20/snake.py
+++ Day 20/snake.py
@@ -23,6 +23,13 @@ class Snake:
         snake.goto(position)
         self.segments.append(snake)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
