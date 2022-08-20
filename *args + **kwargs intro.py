dd9ffc2619b8bcef17733fe70ca707e126d64351
Anietie Akpanusoh <105338623+aniusoro@users.noreply.github.com>
2022-08-20 14:47:58 -0600

    Update *args + **kwargs intro.py

--- *args + **kwargs intro.py
+++ *args + **kwargs intro.py
@@ -18,3 +18,21 @@ def calculate(n, **kwargs):
     n += kwargs["add"]
     n *= kwargs["multiply"]
     print(n)
+
+class Car:
+
+    def __init__(self, **kw):
+        # using the get() function, we can get hold of the value
+        # when using get(), if the key is not in the dictionary, it will just return none
+        # this is important because we can initialise them like this as well:
+        # self.make = kw["make"]
+        # self.make = kw["model]
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+
+
+my_car = Car(make="Toyota")
+# if I did not use the get() function and asked the computer to print "my_car.model" without specifying
+# then the computer would return an error.
+# the get() function allows us the privilege of not having to specify an aforementioned parameter. 
+print(my_car.model)
