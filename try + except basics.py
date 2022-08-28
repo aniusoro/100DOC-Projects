09b5c5d86446983af12db237fdfe819abc52f10f
Anietie Akpanusoh <105338623+aniusoro@users.noreply.github.com>
2022-08-27 22:46:56 -0600

    Update and rename try + except basics.txt to try + except basics.py

--- try + except basics.py
+++ try + except basics.py
@@ -1,4 +1,12 @@
+"""
 try: this is trying something that may cause an exception
+
 except: do this if there was an exception
+  except Keyerror as error_message:
+      print(f"This key {error_message} does not exist")
+this would print the key that does not exist as the "error_message"
+
 else: do this if there were no exceptions
 finally: do this no matter what happens
+
+"""
