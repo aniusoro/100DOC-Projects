6bd39b3a2fb94733619419a9aac92ca93f47a970
Anietie Akpanusoh <105338623+aniusoro@users.noreply.github.com>
2022-09-05 14:09:57 -0600

    Create automatedbirthdaywisher.py

---
+++ Day 32/automatedbirthdaywisher.py
@@ -0,0 +1,31 @@
+import datetime as dt
+import pandas
+import random
+import smtplib
+
+my_email = "udemyday32@yahoo.com"
+password = "stmp00032"
+"""Creating a tuple from today's month and day using datetime."""
+today = (dt.datetime.now().month, dt.datetime.now().day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+
+if today in birthdays_dict:
+    birthday_person = birthdays_dict[today]
+    file_path = f"letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+        connection.starttls()
+        connection.login(my_email, password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
+
+
+
